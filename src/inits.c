f81fc0f904d3635fcd090473eabed69442323ed9
tomas <tomas>
1998-05-20 20:54:32 +0000

    linea removed in oFBis - added init_workstation() and copy_workstation()

--- src/inits.c
+++ src/inits.c
@@ -24,6 +24,94 @@
 
 /* Initialization functions */
 
+void init_workstation(VDI_Workstation *wk)
+{
+  int i;
+  int palsize = ((1 << wk->fb->vinf.red.length) *
+		 (1 << wk->fb->vinf.green.length) *
+		 (1 << wk->fb->vinf.blue.length));
+
+  /* Setup data for v_opnwk */
+
+  wk->dev.attr.xres = wk->fb->vinf.xres_virtual - 1;	/* Max addressable width */
+  wk->dev.attr.yres = wk->fb->vinf.yres_virtual - 1;	/* Max addressable height */
+  wk->dev.attr.noscale = 0;				/* Device Coord Units flag */
+  wk->dev.attr.wpixel = 372;				/* Width of pixel in microns */
+  wk->dev.attr.hpixel = 372;				/* Height of pixel in microns */
+  wk->dev.attr.cheights = 3;				/* Number of different text sizes */
+  wk->dev.attr.linetypes = 0;				/* Number of line types */
+  wk->dev.attr.linewidths = 0;				/* Number of line widths */
+  wk->dev.attr.markertypes = 6;				/* Number of marker types */
+  wk->dev.attr.markersizes = 0;				/* Number of marker sizes */
+  wk->dev.attr.faces = 1;				/* Number of fonts on device */
+  wk->dev.attr.patterns = 24;				/* Number of patterns */
+  wk->dev.attr.hatches = 12;				/* Number of hatch styles */
+  wk->dev.attr.colors = 1 << wk->fb->vinf.bits_per_pixel; /* Number of colours, virt pens (TC) */
+  wk->dev.attr.ngdps = 10;				/* Number of GDPs */
+  for ( i = 0 ; i < 10 ; i++ ) {
+    wk->dev.attr.cangdps[i] = i+1;			/* List of GDPs supported */
+  }
+  wk->dev.attr.gdpattr[0] = 3;				/* GDP attributes */
+  wk->dev.attr.gdpattr[1] = 0;
+  wk->dev.attr.gdpattr[2] = 3;
+  wk->dev.attr.gdpattr[3] = 3;
+  wk->dev.attr.gdpattr[4] = 3;
+  wk->dev.attr.gdpattr[5] = 0;
+  wk->dev.attr.gdpattr[6] = 3;
+  wk->dev.attr.gdpattr[7] = 0;
+  wk->dev.attr.gdpattr[8] = 3;
+  wk->dev.attr.gdpattr[9] = 2;
+  wk->dev.attr.cancolor = ( wk->fb->vinf.bits_per_pixel > 1 ) ? 1 : 0; /* Colour capability */
+  wk->dev.attr.cantextrot = 0;				/* Text rotation */
+  wk->dev.attr.canfillarea = 0;				/* Flag fill-out range */
+  wk->dev.attr.cancellarray = 0;			/* Flag function cell-array */
+  wk->dev.attr.palette = (palsize>32767)? 0 : palsize;	/* Number of available colours */
+  wk->dev.attr.locators = 2;				/* Graphic cursor control */
+  wk->dev.attr.valuators = 1;				/* Number-changeable inputs */
+  wk->dev.attr.choicedevs = 1;				/* Key choice */
+  wk->dev.attr.stringdevs = 1;				/* Alphanumeric input */
+  wk->dev.attr.wstype = 2;				/* Type of work device */
+
+  wk->dev.attr.minwchar = 6;				/* Minimum character width */
+  wk->dev.attr.minhchar = 6;				/* Minimum character height */
+  wk->dev.attr.maxwchar = 8;				/* Maximum character width */
+  wk->dev.attr.maxhchar = 16;				/* Maximum character height */
+  wk->dev.attr.minwline = 1;				/* Minimum line width */
+  wk->dev.attr.zero5 = 0;				/* reserved */
+  wk->dev.attr.maxwline = 1;				/* Maximum line width */
+  wk->dev.attr.zero7 = 0;				/* reserved */
+  wk->dev.attr.minwmark = 1;				/* Minimum marker width */
+  wk->dev.attr.minhmark = 1;				/* Minimum marker height */
+  wk->dev.attr.maxwmark = -1;				/* Maximum marker width */
+  wk->dev.attr.maxhmark = -1;				/* Maximum marker height */
+
+
+  /* Setup data for vq_extnd() */
+
+  wk->inq.attr.screentype = 4;				/* screen type */
+  wk->inq.attr.bgcolors = (palsize>32767)? 0 : palsize;	/* number of bg colours */
+  wk->inq.attr.textfx = -1;				/* text effects */
+  wk->inq.attr.canscale = 0;				/* enlarging raster */
+  wk->inq.attr.planes = wk->fb->vinf.bits_per_pixel;	/* num of planes */
+  wk->inq.attr.lut = 1;					/* look up table */
+  wk->inq.attr.rops = 100;				/* 16*16 rops per second - no idea! */
+  wk->inq.attr.cancontourfill = 0;			/* contour fill */
+  wk->inq.attr.textrot = 0;				/* text rotation */
+  wk->inq.attr.writemodes = 4;				/* num of drawing modes */
+  wk->inq.attr.inputmodes = 2;				/* highest possible input mode */
+  wk->inq.attr.textalign = 0;				/* text alignment */
+  wk->inq.attr.inking = 0;				/* colour change with pens */
+  wk->inq.attr.rubberbanding = 0;			/* colour change with band shift */
+  wk->inq.attr.maxvertices = -1;			/* max points in pline,pmarker,farea */
+  wk->inq.attr.maxintin = -1;				/* max len of intin */
+  wk->inq.attr.mousebuttons = 2;			/* number of mouse buttons */
+  wk->inq.attr.widestyles = 0;				/* line types for wide lines */
+  wk->inq.attr.widemodes = 0;				/* drawing modes for wide lines */
+  for ( i = 0 ; i < 25+12 ; i++ ) {
+    wk->inq.attr.reserved[i] = 0;			/* clear reserved elements */
+  }
+}
+
 void init_cmap(VDI_Workstation *wk)
 {
   int i, ti;
@@ -170,7 +258,20 @@ void init_text(VDI_Workstation *wk)
 
 
 
-/* Copy functions, might seem pointless at the moment */
+/* Copy functions */
+
+void copy_workstation(VDI_Workstation *wk1, VDI_Workstation *wk2)
+{
+  int i;
+
+  /* v_opnvwk values */
+  for (i = 0; i < 45+12; i++)
+    wk2->dev.tab[i] = wk1->dev.tab[i];
+
+  /* vq_extnd values */
+  for (i = 0; i < 45+12; i++)
+    wk2->inq.tab[i] = wk1->inq.tab[i];
+}
 
 void copy_cmap(VDI_Workstation *wk1, VDI_Workstation *wk2)
 {
--- src/inits.h
+++ src/inits.h
@@ -15,12 +15,14 @@
 #ifndef _INITS_H_
 #define _INITS_H_
 
+void init_workstation(VDI_Workstation *);
 void init_cmap(VDI_Workstation *);
 void init_marker(VDI_Workstation *);
 void init_line(VDI_Workstation *);
 void init_fill(VDI_Workstation *);
 void init_text(VDI_Workstation *);
 
+void copy_workstation(VDI_Workstation *, VDI_Workstation *);
 void copy_cmap(VDI_Workstation *, VDI_Workstation *);
 void copy_marker(VDI_Workstation *, VDI_Workstation *);
 void copy_line(VDI_Workstation *, VDI_Workstation *);
