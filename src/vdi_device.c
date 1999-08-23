4e464fcea906bfa5dff89a8028316c5dec686a57
tomas <tomas>
1999-08-23 21:37:47 +0000

    * Files containing device functions, {vsm,vrq}_{string,locator,choice,valuator}   and vsin_mode. Only vsm_string and vsin_mode implemented so far.

---
+++ src/vdi_device.c
@@ -0,0 +1,86 @@
+/*
+ * vdi_device.c
+ *
+ * Copyright 1999 Tomas Berndtsson <tomas@nocrew.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *  
+ * Read the file COPYING for more information.
+ *
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+
+#include "ovdisis.h"
+#include "vdi_device.h"
+
+/* defined in event.c */
+extern pthread_cond_t key_cond;
+extern pthread_mutex_t key_mutex;
+extern int *key_scancode_buffer, *key_ascii_buffer;
+extern int key_first_index, key_amount, key_buffer_length;
+
+
+void vdi_vsm_string(VDI_Workstation *vwk)
+{
+  int maxlen, amount, i;
+  int *buf;
+
+  /* There is echo info in there too, but we don't support that yet, 
+   * if ever. 
+   */
+
+  pthread_mutex_lock(&key_mutex);
+  if(vwk->device_mode[STRING] == REQUEST_MODE)
+    pthread_cond_wait(&key_cond, &key_mutex);
+
+  if(key_first_index >= 0) {
+
+    maxlen = (int)vdipb->intin[0];
+    if(maxlen < 0) {
+      buf = key_scancode_buffer;
+      maxlen = -maxlen;
+    } else {
+      buf = key_ascii_buffer;
+    }
+    
+    amount = min(NR_INTOUT, min(maxlen, key_amount));
+    for(i = 0 ; i < amount ; i++) {
+      vdipb->intout[i] = (WORD)buf[key_first_index];
+      key_first_index = (key_first_index+1) % key_buffer_length;
+    }
+    key_amount -= amount;
+  } else {
+    amount = 0;
+  }
+
+  pthread_mutex_unlock(&key_mutex);
+
+  vdipb->contrl[N_INTOUT] = amount;
+}
+
+/* 
+ * Also in this file, later:
+ * vsm_choice
+ * vsm_locator
+ * vsm_valuator
+ */
+
+void vdi_vsin_mode(VDI_Workstation *vwk)
+{
+  int device, mode;
+
+  device = (int)vdipb->intin[0];
+  mode = (int)vdipb->intin[1];
+
+  if(device >= 1 && device <= 4)
+    vwk->device_mode[device] = mode;
+
+  vdipb->intout[0] = (WORD)mode;
+  vdipb->contrl[N_PTSOUT] = 0;
+  vdipb->contrl[N_INTOUT] = 1;
+}
---
+++ src/vdi_device.h
@@ -0,0 +1,21 @@
+/*
+ * vdi_device.h
+ *
+ * Copyright 1999 Tomas Berndtsson <tomas@nocrew.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *  
+ * Read the file COPYING for more information.
+ *
+ */
+
+#ifndef _VDI_DEVICE_H_
+#define _VDI_DEVICE_H_
+
+extern void vdi_vsm_string(VDI_Workstation *);
+extern void vdi_vsin_mode(VDI_Workstation *);
+
+#endif /* _VDI_DEVICE_H_ */
