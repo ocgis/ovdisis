40fd22c0fe5bdf631533c037d9d3f9c5a5b65f1e
cg <cg>
2000-11-28 17:25:29 +0000

    Added dummy MicroWindows target.

--- acconfig.h
+++ acconfig.h
@@ -5,3 +5,9 @@
 
 /* Define if you have the <ggi/ggi.h> header file.  */
 #undef HAVE_GGI_H
+
+/* Define if you have the microwindows library (-lmw).  */
+#undef HAVE_LIBMW
+
+/* Define if you have the <mw/mw.h> header file.  */
+#undef HAVE_MW_H
---
+++ src/visuals/mw/mw_visual.h
@@ -0,0 +1,13 @@
+/*
+** mw_visual.h
+**
+** Copyright 2000 Christer Gustavsson <cg@nocrew.org>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**  
+** Read the file COPYING for more information.
+**
+*/
